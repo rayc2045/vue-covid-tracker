0f5d17901fd2e3d7a0ed6a2a9fd860be6d0ed6f9
Ray C <rayc2045@gmail.com>
2021-04-24 13:35:31 +0800

    Add header component

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<template>
+  <header class="mb-10 p-4 text-center text-white bg-blue-800">
+    <div class="mb-3 text-3xl md:text-5xl font-bold">
+      <i class="fa fa-viruses"></i>Covid-19 Tracker
+    </div>
+    <p>
+      API by
+      <a class="text-blue-300" href="https://covid19api.com" target="_blank"
+        >covid19api.com</a
+      >
+    </p>
+  </header>
+</template>

10ed7a199bd4b3c5c287104180bbf2a3044e109c
Ray C <rayc2045@gmail.com>
2021-04-27 15:15:17 +0800

    Add country select

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  name: 'CountrySelect',
+  props: ['countries'],
+  data() {
+    return {
+      selected: 0,
+    };
+  },
+  methods: {
+    onChange() {
+      console.log(this.selected);
+      const country = this.countries.find((item) => item.ID === this.selected);
+      console.log(country);
+      this.$emit('get-country', country);
+    },
+  },
+};
+</script>
+
+<template>
+  <select
+    class="form-select mt-1 mb-10 block w-full border p-3 rounded"
+    v-model="selected"
+    @change="onChange()"
+  >
+    <option value="0">Select Country</option>
+    <option v-for="country in countries" :value="country.ID">{{
+      country.Country
+    }}</option>
+  </select>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,14 @@
 <script>
 import DataTitle from '@/components/DataTitle';
 import DataBoxes from '@/components/DataBoxes';
+import CountrySelect from '@/components/CountrySelect';
 
 export default {
   name: 'Home',
   components: {
     DataTitle,
-    DataBoxes
+    DataBoxes,
+    CountrySelect,
   },
   data() {
     return {
@@ -24,6 +26,10 @@ export default {
       const data = await res.json();
       return data;
     },
+    getCountryData(country) {
+      this.stats = country;
+      this.title = country.Country;
+    },
   },
   async created() {
     const data = await this.fetchCovidData();
@@ -37,7 +43,8 @@ export default {
 
 <template>
   <main v-if="!loading" class="">
-    <DataTitle :text="title" :dataDate="dataDate"/>
+    <CountrySelect :countries="countries" @get-country="getCountryData" />
+    <DataTitle :text="title" :dataDate="dataDate" />
     <dataBoxes :stats="stats" />
   </main>
 
